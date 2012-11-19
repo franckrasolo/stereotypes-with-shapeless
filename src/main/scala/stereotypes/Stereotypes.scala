9892eb4bce7fb5402efdb2fb982d7f9c83ff6d58
Franck Rasolo <franck.rasolo@gmail.com>
2012-11-19 21:30:07 +0000

    Updated as suggested by Travis Brown in https://groups.google.com/d/msg/shapeless-dev/hn7_U21tupI/o3-AMMBhHJYJ

--- src/main/scala/stereotypes/Stereotypes.scala
+++ src/main/scala/stereotypes/Stereotypes.scala
@@ -5,12 +5,11 @@ case class Stereotype(name: String, description: String, aliases: List[String])
 trait Stereotypes {
   import shapeless._, Searchable._
 
+  def find(term: String): Either[String, Stereotype] = find(_ equalsIgnoreCase term).toRight(message(term))
   val stereotypes: List[Stereotype]
-  private implicit def stereotypeIso = Iso.hlist(Stereotype.apply _, Stereotype.unapply _)
 
-  def find(term: String): Either[String, Stereotype] = stereotypes
-    .deepFind((_: Stereotype).deepFind((_: String) equalsIgnoreCase term).isDefined)
-    .toRight(message(term))
+  private implicit def stereotypeIso = Iso.hlist(Stereotype.apply _, Stereotype.unapply _)
+  private def find(predicate: String => Boolean) = stereotypes.find(_.deepFind(predicate).isDefined)
 
   private def message(term: String) = "Invalid Stereotype [%s]. Must be one of [%s]".format(term, allowedTerms)
   private lazy val allowedTerms = stereotypes.flatMap(s => s.name :: s.description :: s.aliases).sorted.mkString(", ")
