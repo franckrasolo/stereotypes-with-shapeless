706f91d2273b60b8eb20679692f8758c9400c354
Franck Rasolo <franck.rasolo@gmail.com>
2012-12-04 02:54:42 +0000

    Simplified assertions using ScalaTest's MustMatchers and EitherValues

--- src/test/scala/stereotypes/StereotypesSuite.scala
+++ src/test/scala/stereotypes/StereotypesSuite.scala
@@ -1,13 +1,14 @@
 package stereotypes
 
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalatest.{EitherValues, FunSuite}
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.MustMatchers
 
 @RunWith(classOf[JUnitRunner])
 class StereotypesSuite extends FunSuite {
 
-  trait Examples extends Stereotypes {
+  trait Examples extends Stereotypes with MustMatchers with EitherValues {
     val australia  = Stereotype("australia",  "a person from australia",   List("aussie", "legend"))
     val newZealand = Stereotype("newzealand", "a person from new zealand", List("kiwi"))
     val preston    = Stereotype("preston",    "a person from preston",     List("scally"))
@@ -20,38 +21,36 @@ class StereotypesSuite extends FunSuite {
 
   test("find by description") {
     new Examples {
-      assert(find("a person from new zealand") === Right(newZealand))
+      find("a person from new zealand").right.value must be === newZealand
     }
   }
 
   test("find by name") {
     new Examples {
-      assert(find("manchester") === Right(manchester))
+      find("manchester").right.value must be === manchester
     }
   }
 
   test("find by alias") {
     new Examples {
-      assert(find("scouser") === Right(liverpool))
+      find("scouser").right.value must be === liverpool
     }
   }
 
   test("find is case insensitive") {
     new Examples {
-      assert(find("scally") === Right(preston))
-      assert(find("SCALLY") === Right(preston))
-      assert(find("ScAlLy") === Right(preston))
+      find("scally").right.value must be === preston
+      find("SCALLY").right.value must be === preston
+      find("ScAlLy").right.value must be === preston
     }
   }
 
   test("find returns a descriptive message when no stereotype is found") {
     new Examples {
-      assert(find("Awesome") === Left(
-        "Invalid Stereotype [Awesome]. Must be one of [a person from australia, " +
-          "a person from liverpool, a person from manchester, a person from new zealand, " +
-          "a person from preston, aussie, australia, fighter, kiwi, legend, liverpool, " +
-          "manc, manchester, mancunian, newzealand, preston, scally, scouser, thief]"
-      ))
+      find("Awesome").left.value must be === "Invalid Stereotype [Awesome]. Must be one of [a person from australia, " +
+        "a person from liverpool, a person from manchester, a person from new zealand, " +
+        "a person from preston, aussie, australia, fighter, kiwi, legend, liverpool, " +
+        "manc, manchester, mancunian, newzealand, preston, scally, scouser, thief]"
     }
   }
 }
